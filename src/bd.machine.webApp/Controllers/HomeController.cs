1fc521e09dc01b52d2f7a21c30d36f49df72a6d3
Stefan Merkel <merkel.s.ulm@gmail.com>
2017-01-19 11:06:50 +0100

    update

--- src/bd.machine.webApp/Controllers/HomeController.cs
+++ src/bd.machine.webApp/Controllers/HomeController.cs
@@ -1,14 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Mvc.Ajax;
-
-namespace bd.machine.webApp.Controllers
+﻿namespace bd.machine.webApp.Controllers
 {
+	using System;
+	using System.Web.Mvc;
+	using bal.Interfaces;
+	using bal.Implementations;
+	using dal.Implementations;
+	using dal.Interfaces;
+
 	public class HomeController : Controller
 	{
+		private readonly IUrlService _urlService;
+		
+		public HomeController(IContext context) 
+		{
+			if (context == null)
+				throw new ArgumentNullException("context");
+			_urlService = new UrlService(new UnitOfWork(context));
+		}
+
+		public HomeController() : this(new Context("name=MySql")) { }
+		
 		public ActionResult Index()
 		{
 			var mvcName = typeof(Controller).Assembly.GetName();
@@ -16,6 +27,7 @@ namespace bd.machine.webApp.Controllers
 
 			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
 			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+			ViewData["Uris"] = _urlService.GetAllUris()[2];
 
 			return View();
 		}
