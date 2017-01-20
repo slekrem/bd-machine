5f85ac4cf94a56b86fe5d73bec55b196fd800728
Stefan Merkel <merkel.s.ulm@gmail.com>
2017-01-20 23:36:46 +0100

    create CrawlableUrlEntity

---
+++ src/dal/Implementations/Entities/CrawlableUrlEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace bd.machine.dal.Implementations.Entities
+{
+	using System;
+	using System.ComponentModel.DataAnnotations;
+	using System.ComponentModel.DataAnnotations.Schema;
+
+	[Table("CrawlableUrl")]
+	public class CrawlableUrlEntity
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public int RawUrlId { get; set; }
+
+		public DateTime Timestamp { get; set; }
+
+		public bool IsActivated { get; set; }
+
+		public virtual RawUrlEntity RawUrl { get; set; }
+	}
+}
