7bd22006590d9351c1b2f4bc24e71f6b5246e506
Stefan Merkel <merkel.s.ulm@gmail.com>
2017-01-24 23:55:56 +0100

    update

---
+++ src/webApp/Controllers/HostController.cs
@@ -0,0 +1,12 @@
+﻿namespace bd.machine.webApp.Controllers
+{
+	using System.Web.Mvc;
+
+	public class HostController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
